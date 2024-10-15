a31e00c4b07d21faa743ecc21f70ad5c32a9ce8c
AlexMoreni <alex.moreni47@gmail.com>
2024-10-15 09:38:31 -0300

    chore: setup drizzle and neon DB

---
+++ db/migrate.ts
@@ -0,0 +1,24 @@
+import { neon } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http"
+import { migrate } from "drizzle-orm/neon-http/migrator"
+
+import "dotenv/config"
+
+const sql = neon(process.env.DATABASE_URL!)
+
+const db = drizzle(sql)
+
+const main = async () => {
+  try {
+    await migrate(db, { migrationsFolder: "./db/migrations" })
+
+    console.log("Migration successful!")
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+main()
+
+export default db
